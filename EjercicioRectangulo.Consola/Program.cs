f26d7bda117852747ae6b4105a5e0bce1aabc1a0
camila <camila@LAPTOP-N0GHACTF>
2022-08-22 23:38:19 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioRectangulo.Consola/Program.cs
@@ -0,0 +1,41 @@
+﻿using EjercicioRectangulo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioRectangulo.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ladoa=1, ladob=1, ladoc=1;
+            Triangulo triangulo = new Triangulo(ladoa,ladob,ladoc);
+            Console.Write("ingrese el ladoA:");
+            ladoa = int.Parse(Console.ReadLine());
+            Console.Write("ingrese el ladoB:");
+            ladob = int.Parse(Console.ReadLine());
+            Console.Write("ingrese el ladoC:");
+            ladoc = int.Parse(Console.ReadLine());
+
+            if (Triangulo.Validar())
+            {
+                Triangulo triangulo1 = new Triangulo(ladoa, ladob, ladoc);
+                Console.WriteLine($"{triangulo1.GetTriangulo()}");
+                Console.WriteLine($"perimetro:{Triangulo.GetPerimetro()}");
+                Console.WriteLine($"semiperimetro:{Triangulo.GetSemiPerimetro()}");
+                Console.WriteLine($"superficie:{Triangulo.GetSuperficie()}");
+            }
+            else
+            {
+                Console.WriteLine("lados mal ingresados");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ EjercicioRectangulo.Entidades/Triangulo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioRectangulo.Entidades
+{
+    public  class Triangulo
+    {
+        public static int LadoA { get; set; }
+        public static int LadoB { get; set; }
+        public static int LadoC { get; set; }
+        public  Triangulo(int ladoa,int ladob,int ladoc)
+        {
+            LadoA = ladoa;
+            LadoB = ladob;
+            LadoC = ladoc;
+        }
+        
+        public static bool Validar()
+        {
+            return LadoA + LadoB > LadoC && LadoA + LadoC > LadoB && LadoB + LadoC > LadoA;       
+        }
+        public string GetTriangulo()
+        {
+            string Resultado = "";
+            if (Math.Pow(LadoA,2)==Math.Pow(LadoB,2)+Math.Pow(LadoC,2))
+            {
+                Resultado = "Se forma un triángulo rectángulo";
+            }
+            else if (Math.Pow(LadoA, 2) > Math.Pow(LadoB, 2) + Math.Pow(LadoC, 2))
+            {
+                Resultado = "Se forma un triángulo obtusángulo";
+            }
+            else if (Math.Pow(LadoA, 2) < Math.Pow(LadoB, 2) + Math.Pow(LadoC, 2))
+            {
+                Resultado = "Se forma un triángulo acutángulo";
+            }
+            return Resultado;
+        }
+        public static double GetPerimetro()
+        {
+            return LadoA + LadoB + LadoC;
+        }
+
+        public static double GetSemiPerimetro()
+        {
+            return (LadoA + LadoB + LadoC) / 2;
+        }
+        public static double GetSuperficie()
+        {
+            return Math.Sqrt(GetSemiPerimetro() * (GetSemiPerimetro() - LadoA) * (GetSemiPerimetro() - LadoB) * (GetSemiPerimetro() - LadoC));
+        }
+    }
+}
